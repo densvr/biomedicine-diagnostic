7619337ed03e06a7c41267c72f2ab80ba275a098
Daniel Sergeev <danser@yandex-team.ru>
2018-04-27 13:38:47 +0300

    refactor code

--- confocal-tissue/TissueML.py
+++ confocal-tissue/TissueML.py
@@ -4,149 +4,16 @@ import random
 import cv2
 import csv
 import numpy as np
-# Standard scientific Python imports
 import matplotlib.pyplot as plt
-# Import datasets, classifiers and performance metrics
 from sklearn import datasets, svm, metrics, neural_network
 
-# pathToImages = "/home/slobodanka/Documents/masterThesis/CellsProject-master/images/"
 datasetPath = "/Users/danser/Google Drive/post graduate/cell couting on digital microscopy images/projects/biomedicine-diagnostic/dataset/tissue/"
-test = datasetPath + "1 (19).jpg"
-
-dictPhotos = {}
-dictPhotos = {1: 40, 2: 28, 3: 145, 4: 112, 8: 36, 9: 13, 10: 91, 11: 1516, 12: 362, 13: 419, 14: 257, 15: 228, 16: 121,
-              17: 136,
-              18: 110, 19: 856, 20: 819, 21: 964, 22: 885, 23: 928, 28: 915, 29: 770, 30: 164, 33: 43, 34: 23, 37: 44,
-              38: 52, 39: 52,
-              40: 147, 43: 92, 44: 63, 45: 136, 46: 97, 47: 113, 52: 44, 53: 42, 54: 50, 55: 41, 56: 35, 57: 159,
-              58: 131, 60: 95, 61: 102,
-              62: 100, 65: 53, 68: 100, 71: 95, 72: 100, 75: 19, 76: 27, 79: 6, 85: 38, 86: 26, 87: 50, 88: 71, 89: 34,
-              90: 35, 91: 26, 92: 37}
-
-
-def segment(img):
-    new_img = np.copy(img)
-    new_img = np.float64(new_img)
-    pos = new_img > (np.mean(img) + 20)
-    new_img[pos] += 100
-    others = new_img < (np.mean(img) + 20)
-    new_img[new_img > 255] = 255
-    new_img[others] -= 100
-    new_img[new_img < 0] = 0
-    return new_img
-
-
-def threshold(img):
-    img = np.uint8(img)
-    return img
-
-
-def count_cells(img, oimg):
-    ret, thresh = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
-    image, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(oimg, contours, -1, (0, 255, 255), 1)
-    # print("The number of cells are", len(contours))
-
-    count = 0
-    for c in contours:
-        M = cv2.moments(c)
-        denom = M['m00']
-        if denom == 0:
-            denom = 1
-        cX = int(M['m10'] / denom)
-        cY = int(M['m01'] / denom)
-
-        if cX != cY:
-            cv2.drawContours(oimg, [c], -1, (0, 255, 0), 2)
-            count += 1
-    # print('COUNT', count)
-
-    # return oimg
-    return count
-
-
-def process_image(img):
-    oimg = np.copy(img)
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    h, s, v = cv2.split(hsv)
-    img = v
-    # cv2.imshow("Gray2.tif", img)
-    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
-
-    img = clahe.apply(img)
-    #    cv2.imshow('filtered', img)
-
-    kernel = np.ones((3, 3), np.uint8)
-    img = cv2.erode(img, kernel, iterations=1)
-    #  cv2.imshow('erode', img)
-
-    img = segment(img)
-    #   cv2.imshow('segment', img)
-    img = threshold(img)
-    #    cv2.imshow('threshold', img)
-
-    # img = cv2.GaussianBlur(img, (10,10), 0)
-    img = cv2.medianBlur(img, 5)
-    #    cv2.imshow('median', img)
-    numberOfCells = count_cells(img, oimg)
-    # cv2.imshow("final", oimg)
-    k = cv2.waitKey(0)
-
-    return numberOfCells
 
 
 # cv2.destroyAllWindows()
 
-def detectGlands(img):
-    oimg = np.copy(img)
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    h, s, v = cv2.split(hsv)
-    img = v
-    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
-    img = clahe.apply(img)
-    kernel = np.ones((3, 3), np.uint8)
-    img = cv2.erode(img, kernel, iterations=1)
-    img = segment(img)
-    img = threshold(img)
-    img = cv2.medianBlur(img, 5)
-    thresh, binMat = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
-    # image, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    # cv2.drawContours(oimg, contours, -1, (0, 255, 255), 1
 
-    binMat = cv2.dilate(binMat, cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3)), iterations=22)
-    binMat = cv2.erode(binMat, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3)), iterations=33)
-
-    # cv2.imshow("bin", binMat)
-    # cv2.waitKey(0)
-
-    cv2.rectangle(binMat, (0, 0), (np.size(binMat, 0), np.size(binMat, 1)), 255, 10)
-
-    image, contours, hierarchy = cv2.findContours(binMat, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
-
-    for contour in contours:
-
-        contourArea = cv2.contourArea(contour)
-        if contourArea < 100 * 100 or contourArea > 0.9 * np.size(image, 0) * np.size(image, 1):
-            continue
-
-        cv2.drawContours(oimg, [contour], -1, (0.0, 0.0, 255.0), 2)
-
-    cv2.imshow("glands", oimg)
-    cv2.waitKey()
-
-
-def calcPercentage(imgName, numberOfCells):
-    imageNumber = int(imgName.split('.')[0])
-    percentage = ((abs(dictPhotos[imageNumber] - numberOfCells)) / dictPhotos[imageNumber]) * 100
-    # print("ImageNumber: ", imageNumber, " cells: ", dictPhotos[imageNumber], " observed: ",\
-    #      numberOfCells, "percentage:", (100.0 -percentage), "or real:", percentage)
-    print("ImageNumber: ", imageNumber, " cells: ", dictPhotos[imageNumber], " observed: ", \
-          numberOfCells, "percentage:",
-          min(dictPhotos[imageNumber], numberOfCells) / max((dictPhotos[imageNumber], numberOfCells)))
-    return min(dictPhotos[imageNumber], numberOfCells) / max((dictPhotos[imageNumber], numberOfCells))
-
-
-def generateSamples(image, gland, countInside, countOutside, sampleSize, sampleResizedSize):
+def generateSamples(image, gland, countInside, countOutside, sampleSize, sampleResizedSize, isDebug: bool):
     nparr = np.array(gland[0])
     contourMask: np.ndarray = np.zeros((image.shape[0], image.shape[1], 1), np.uint8)
     cv2.drawContours(contourMask, [nparr], 0, color=255, thickness=-1)
@@ -162,26 +29,26 @@ def generateSamples(image, gland, countInside, countOutside, sampleSize, sampleR
         croppedImage = cropImage(image, rect)
         croppedImage = cv2.resize(croppedImage, sampleResizedSize)
 
-        # imgDraw = image.copy()
+        imgDraw = image.copy()
 
         if contourMask[int(y + sampleSize[1] / 2), int(x + sampleSize[0] / 2)] > 0:
             if np.size(insideImages, 0) < countInside:
                 insideImages += [croppedImage]
                 insideRects += [rect]
-                # cv2.rectangle(imgDraw, (rect[0], rect[2]), (rect[0] + rect[1], rect[2] + rect[3]), (0, 0, 255), 3)
-                # imgDraw = cv2.resize(imgDraw, (600, 600))
-                # cv2.imshow("cropped", croppedImage)
-                # cv2.imshow("contour", imgDraw)##
-                # cv2.waitKey()
+                if isDebug:
+                    cv2.rectangle(imgDraw, (rect[0], rect[2]), (rect[0] + rect[1], rect[2] + rect[3]), (0, 0, 255), 3)
+                    showImage("contour", imgDraw)
+                    cv2.imshow("cropped", croppedImage)
+                    cv2.waitKey()
         else:
             if np.size(outsideImages, 0) < countOutside:
                 outsideImages += [croppedImage]
                 outsideRects += [rect]
-                ##cv2.rectangle(imgDraw, (rect[0], rect[2]), (rect[0] + rect[1], rect[2] + rect[3]), (0, 255, 0), 3)
-                ##imgDraw = cv2.resize(imgDraw, (600, 600))
-                ##cv2.imshow("cropped", croppedImage)
-                ##cv2.imshow("contour", imgDraw)
-                ##cv2.waitKey()
+                if isDebug:
+                    cv2.rectangle(imgDraw, (rect[0], rect[2]), (rect[0] + rect[1], rect[2] + rect[3]), (0, 255, 0), 3)
+                    showImage("contour", imgDraw)
+                    cv2.imshow("cropped", croppedImage)
+                    cv2.waitKey()
 
     return insideImages, outsideImages, insideRects, outsideRects
 
@@ -198,6 +65,11 @@ def cropImage(image, rect):
 
 def flattenImage(image):
     return np.reshape(image, -1)
+
+
+def showImage(name, img):
+    cv2.resize(img, (600, 600))
+    cv2.imshow(name, img)
 
 
 def main():
@@ -240,16 +112,16 @@ def main():
             cv2.drawContours(imgWithLabelledGlands, [nparr], 0, (0, 255, 0), 4)
             nparr = np.array(gland[1])
             cv2.drawContours(imgWithLabelledGlands, [nparr], 0, (0, 255, 255), 4)
-        #imgWithLabelledGlands = cv2.resize(imgWithLabelledGlands, (600, 600))
-        #cv2.imshow(imgName + " labelled glands", imgWithLabelledGlands)
+
+        showImage(imgName + " labelled glands", imgWithLabelledGlands)
 
         # generate images
         insideImages, outsideImages, insideRects, outsideRects = generateSamples(srcImg, glands[0], 500, 500,
-                                                                                 (100, 100), (10, 10))
+                                                                                 (100, 100), (10, 10), False)
 
         insideSamples = flattenSamples(insideImages)
         outsideSamples = flattenSamples(outsideImages)
-        ##
+
         classifier = neural_network.MLPClassifier(hidden_layer_sizes=(100,))
 
         classifier.fit(insideSamples + outsideSamples,
@@ -258,7 +130,7 @@ def main():
         while 1:
 
             insideImages, outsideImages, insideRects, outsideRects = generateSamples(srcImg, glands[0], 100, 100,
-                                                                                     (100, 100), (10, 10))
+                                                                                     (100, 100), (10, 10), False)
             insideSamples = flattenSamples(insideImages)
             outsideSamples = flattenSamples(outsideImages)
 
@@ -277,8 +149,7 @@ def main():
                     cv2.rectangle(imgDraw, (rect[0], rect[2]), (rect[0] + rect[1], rect[2] + rect[3]), (0, 0, 255), 2)
                 count += 1
 
-            imgDraw = cv2.resize(imgDraw, (600, 600))
-            cv2.imshow("result", imgDraw)
+            showImage("result", imgDraw)
 
             k = cv2.waitKey()
             if k == 27:  # Esc key to stop
